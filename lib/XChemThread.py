7945ee035e9766330e52bb813dda61e373793f7d
tkrojer <tobias.krojer@sgc.ox.ac.uk>
2015-11-18 07:47:04 +0000

    18/11/2015

--- lib/XChemThread.py
+++ lib/XChemThread.py
@@ -245,8 +245,8 @@ class read_autoprocessing_results_from_disc(QtCore.QThread):
                     if run_is_in_pickle_file:
                         for stuff in self.data_collection_dict_collected[xtal][0]:
                             if stuff[0]==run:
-                                self.data_collection_dict[xtal][0]+=stuff
-                                #run_list.append(stuff)
+                                #self.data_collection_dict[xtal][0]+=stuff
+                                run_list.append(stuff)
                         for stuff in self.data_collection_dict_collected[xtal][1]:
                             if run in stuff:
                                 #self.data_collection_dict[xtal][1]+=stuff
