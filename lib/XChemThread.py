ee68341a4404c8b467b1b548176f22db58a1e414
tkrojer <tobias.krojer@sgc.ox.ac.uk>
2015-11-18 07:19:46 +0000

    18/11/2015

--- lib/XChemThread.py
+++ lib/XChemThread.py
@@ -251,8 +251,8 @@ class read_autoprocessing_results_from_disc(QtCore.QThread):
                                 self.data_collection_dict[xtal][1]+=stuff
                         for stuff in self.data_collection_dict_collected[xtal][2]:
                             if run in stuff:
-                                print stuff
                                 self.data_collection_dict[xtal][2]+=stuff
+                        print self.data_collection_dict[xtal][2]
                         for stuff in self.data_collection_dict_collected[xtal][3]:
                             if run in stuff[0]:
                                 self.data_collection_dict[xtal][3]+=stuff
