30e8f4afe6fc5624c4073ab587fca233458becae
Tobias Krojer <tobiaskrojer@Tobiass-MacBook-Air.local>
2015-12-11 14:01:42 +0000

    11/12/2015

--- lib/XChemUtils.py
+++ lib/XChemUtils.py
@@ -8,15 +8,15 @@ import subprocess
 import getpass
 import shutil
 import platform
-if os.getenv('PHENIX'):
-    if platform.system()=='Linux':
-        sys.path.append(os.path.join(os.getenv('PHENIX'),'build','intel-linux-2.6-x86_64','base','lib','python2.7','site-packages'))
-    if platform.system()=='Darwin':
-        sys.path.append(os.path.join(os.getenv('PHENIX'),'base','Python.framework','Versions','2.7','lib','python2.7','site-packages'))
-    import PIL
-    from rdkit import Chem
-    from rdkit.Chem import AllChem
-    from rdkit.Chem import Draw
+#if os.getenv('PHENIX'):
+#    if platform.system()=='Linux':
+#        sys.path.append(os.path.join(os.getenv('PHENIX'),'build','intel-linux-2.6-x86_64','base','lib','python2.7','site-packages'))
+#    if platform.system()=='Darwin':
+#        sys.path.append(os.path.join(os.getenv('PHENIX'),'base','Python.framework','Versions','2.7','lib','python2.7','site-packages'))
+#    import PIL
+#    from rdkit import Chem
+#    from rdkit.Chem import AllChem
+#    from rdkit.Chem import Draw
 
 # last commited: 03/12/2015
 
