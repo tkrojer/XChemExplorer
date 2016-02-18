dd9b4690899243e24c2e2fabd782099c6cc32a42
Tobias Krojer <tobiaskrojer@Tobiass-MacBook-Air.local>
2016-02-18 17:34:16 +0000

    18/02/2016 -

--- lib/XChemThread.py
+++ lib/XChemThread.py
@@ -786,8 +786,8 @@ class temp_read_autoprocessing_results_from_disc(QtCore.QThread):
         self.data_collection_dict={}
         self.database_directory=database_directory
 
-        if os.path.isfile(os.path.join(self.database_directory,'data_collection_summary.pkl')):
-            self.data_collection_dict = pickle.load( open( os.path.join(self.database_directory,'data_collection_summary.pkl'), "rb" ) )
+#        if os.path.isfile(os.path.join(self.database_directory,'data_collection_summary.pkl')):
+#            self.data_collection_dict = pickle.load( open( os.path.join(self.database_directory,'data_collection_summary.pkl'), "rb" ) )
 
         # - open data source if possible
         # - get sampleID, xtbm
