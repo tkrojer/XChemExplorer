1410c3b2a494cd199f6c0a4a77a35dfb3c49a85c
tkrojer <tobias.krojer@sgc.ox.ac.uk>
2016-03-04 09:56:47 +0000

    04/03/2016 - 34

--- lib/XChemThread.py
+++ lib/XChemThread.py
@@ -888,12 +888,11 @@ class NEW_read_autoprocessing_results_from_disc(QtCore.QThread):
                             if entry[0]=='image' and entry[1]==visit and entry[2]==run:
                                 image_files_in_list=True
                             if not image_files_in_list:
-                                print 'ydeyge',entry[6]
-                                run_number_list.append(entry[6])
+                                run_number_list.append(int(entry[6])+1)
                     if run_number_list==[]:
                         run_number=1
-                    else:
-                        run_number=max(run_number_list)+1
+#                    else:
+#                        run_number=max(run_number_list)+1
                     if not image_files_in_list:
                         image_list=[]
                         for image in glob.glob(os.path.join(visit_directory,'jpegs',self.target,xtal,'*')):
