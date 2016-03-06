d2e6c223578a18e6218742eb547b2e69bee86d7d
Tobias Krojer <tobiaskrojer@Tobiass-Air.home>
2016-03-06 09:34:09 +0000

    06/03/2016 - 3

--- XChemExplorer_NEW.py
+++ XChemExplorer_NEW.py
@@ -1974,7 +1974,7 @@ class XChemExplorer(QtGui.QApplication):
                     image_number+=1
 
                 elif header.startswith('Show'):
-                    start_albula_button=QtGui.QPushButton('Show: '+str(latest_run[5])[str(latest_run[5].rfind('/')+1:)]
+                    start_albula_button=QtGui.QPushButton('Show: '+str(latest_run[5])[str(latest_run[5].rfind('/')+1:)])
                     start_albula_button.clicked.connect(self.button_clicked)
                     self.albula_button_dict[xtal]=[start_albula_button,diffraction_image]
                     self.data_collection_summary_table.setCellWidget(row,column,start_albula_button)
