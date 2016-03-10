49ae17c985b595ae29fbc38ffc26440a7bbc9ece
Tobias Krojer <tobiaskrojer@Tobiass-MacBook-Air.local>
2016-03-10 16:12:46 +0000

    10/03/2016 - 31

--- XChemExplorer_NEW.py
+++ XChemExplorer_NEW.py
@@ -1881,6 +1881,7 @@ class XChemExplorer(QtGui.QApplication):
 
     def preferences_data_to_copy_combobox_changed(self,i):
         text = str(self.preferences_selection_mechanism_combobox.currentText())
+        print 'changing something'
         for item in self.preferences_data_to_copy:
             if item[0] == text:
                 self.preferences['processed_data_to_copy']=item[1]
