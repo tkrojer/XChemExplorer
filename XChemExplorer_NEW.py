b0f10c3412ede4c515c424a326ca453994ac7887
Tobias Krojer <tobiaskrojer@Tobiass-MacBook-Air.local>
2016-04-12 13:29:36 +0100

    12/04/2016 - 31

--- XChemExplorer_NEW.py
+++ XChemExplorer_NEW.py
@@ -2456,7 +2456,7 @@ class XChemExplorer(QtGui.QApplication):
             if self.data_collection_summary_dict[key][3]==self.sender():
                 if self.sender().isChecked():
                     for item in tmp:
-                        if item[0]==self.sender():
+                        if item[0]==key:
                             for column in self.data_collection_summary_table.columnCount():
                                 self.data_collection_summary_table.item(item[1], column).setBackground(QtGui.QColor(255,255,150))
                     self.data_collection_details_currently_on_display=self.data_collection_column_three_dict[key][0]
