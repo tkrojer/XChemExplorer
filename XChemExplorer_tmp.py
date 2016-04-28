5267d6ab733e6a5012d741af1912a0aa3d1fa5fe
Tobias Krojer <tobiaskrojer@Tobiass-MacBook-Air.local>
2016-04-28 21:18:34 +0100

    28/04/2016 - 28

--- XChemExplorer_tmp.py
+++ XChemExplorer_tmp.py
@@ -1727,6 +1727,7 @@ class XChemExplorer(QtGui.QApplication):
             self.check_for_new_autoprocessing_or_rescore(True)
 
         elif instruction=="Read PKL file":
+            self.status_bar.showMessage('Unpickling '+self.data_collection_summary_file)
             summary = pickle.load( open( self.data_collection_summary_file, "rb") )
             self.create_widgets_for_autoprocessing_results_only(summary)
 
