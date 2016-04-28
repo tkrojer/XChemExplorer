76441446d6670a960af8b534e8c2774b58f8ce5f
Tobias Krojer <tobiaskrojer@Tobiass-MacBook-Air.local>
2016-04-28 07:25:09 +0100

    27/04/2016

---
+++ lib/XChemMain.py
@@ -0,0 +1,38 @@
+import os,glob
+
+def get_target_and_visit_list(beamline_directory):
+    target_list=['*']      # always give the option to read in all targets
+    visit_list=[]
+    # the beamline directory could be a the real directory or
+    # a directory where the visits are linked into
+    if len(beamline_directory.split('/')) and \
+        beamline_directory.split('/')[1]=='dls' and beamline_directory.split('/')[3]=='data' \
+        and not 'labxchem' in beamline_directory:
+        visit_list.append(beamline_directory)
+    else:
+        for dir in glob.glob(beamline_directory+'/*'):
+            visit_list.append(os.path.realpath(dir))
+    for visit in visit_list:
+        for target in glob.glob(os.path.join(visit,'processed','*')):
+            if target[target.rfind('/')+1:] not in ['results','README-log','edna-latest.html']:
+                if target[target.rfind('/')+1:] not in target_list:
+                    target_list.append(target[target.rfind('/')+1:])
+    return target_list,visit_list
+
+def get_dewar_configuration(beamline_directory):
+    dewar_sample_configuration_dict={}
+    # need to do it like this for now until we got a way to query ispyb
+    for xml in glob.glob(os.path.join(beamline_directory,'xml','exptTableParams-*.xml')):
+        prefix=''
+        container_reference=''
+        sample_location=''
+        for line in open(xml):
+            if 'prefix' in line:
+                prefix=line[line.find('>')+1:line.rfind('<')]
+            if 'container_reference' in line:
+                container_reference=line[line.find('>')+1:line.rfind('<')]
+            if 'sample_location' in line:
+                sample_location=line[line.find('>')+1:line.rfind('<')]
+        dewar_sample_configuration_dict[str(container_reference)+'-'+str(sample_location)]=prefix
+    return dewar_sample_configuration_dict
+
