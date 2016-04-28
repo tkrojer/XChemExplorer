a91c19235d9af81c09d55a15491e2cfc1ea3d996
Tobias Krojer <tobiaskrojer@Tobiass-MacBook-Air.local>
2016-04-28 18:12:04 +0100

    28/04/2016 - 13

--- lib/XChemThread.py
+++ lib/XChemThread.py
@@ -1369,6 +1369,7 @@ class NEW_read_autoprocessing_results_from_disc(QtCore.QThread):
             if entry[0]=='logfile':
                 index=self.data_collection_dict[xtal][n][7]
                 print 'index',index
+                print entry[6]
                 if isinstance(entry[6],dict):
                     try:
                         if isinstance(float(entry[6]['DataProcessingUnitCellVolume']),float):
