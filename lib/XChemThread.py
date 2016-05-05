9fdb0bfb0c6a1697b9e286b89576963751e1cf68
Tobias Krojer <tobiaskrojer@Tobiass-MacBook-Air.local>
2016-05-05 14:05:05 +0100

    05/05/2016 - 36

--- lib/XChemThread.py
+++ lib/XChemThread.py
@@ -276,7 +276,7 @@ class run_dimple_on_all_autoprocessing_files(QtCore.QThread):
             Cmds = (
                     '%s\n' %top_line+
                     '\n'
-                    'export XChemExplorer_DIR="os.getenv('+XChemExplorer_DIR+')"\n'
+                    'export XChemExplorer_DIR="'+os.getenv('XChemExplorer_DIR')+'"\n'
                     '\n'
                     'cd %s\n' %os.path.join(self.initial_model_directory,xtal,'dimple',visit_run_autoproc) +
                     '\n'
