020b22984accc97a48ba683852f82241f984a055
Tobias Krojer <tobiaskrojer@Tobiass-MacBook-Air.local>
2016-05-05 13:17:17 +0100

    05/05/2016 - 31

--- helpers/update_data_source_for_new_dimple_pdb.py
+++ helpers/update_data_source_for_new_dimple_pdb.py
@@ -16,12 +16,19 @@ if __name__=='__main__':
     xtal=sys.argv[2]
     inital_model_directory=sys.argv[3]
 
+    print db,xtal,inital_model_directory
+
     print 'XCE path',os.path.join(os.getenv('XChemExplorer_DIR'),'lib')
 
 
 #    db=XChemDB.data_source(db_file)
     if os.path.isfile(os.path.join(inital_model_directory,xtal,'dimple.pdb')):
+        db_dict={}
+        db_dict['DimplePathToPDB']=os.path.join(inital_model_directory,xtal,'dimple.pdb')
+        if os.path.isfile(os.path.join(inital_model_directory,xtal,'dimple.mtz')):
+            db_dict['DimplePathToMTZ']=os.path.join(inital_model_directory,xtal,'dimple.mtz')
         print os.path.join(inital_model_directory,xtal,'dimple.pdb')
         pdb=parse().PDBheader(os.path.join(inital_model_directory,xtal,'dimple.pdb'))
-        print pdb['Rcryst']
-        print pdb['Rfree']
+        db_dict['DimpleRcryst']=pdb['Rcryst']
+        db_dict['DimpleRfree']=pdb['Rfree']
+        print db_dict
