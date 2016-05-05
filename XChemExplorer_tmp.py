6dee1a13fbdf7f83ea05062407c44676132c9885
Tobias Krojer <tobiaskrojer@Tobiass-MacBook-Air.local>
2016-05-05 17:16:47 +0100

    05/05/2016 - 51

--- XChemExplorer_tmp.py
+++ XChemExplorer_tmp.py
@@ -2451,10 +2451,11 @@ class XChemExplorer(QtGui.QApplication):
                                 reference_file_selection_combobox.setCurrentIndex(index)
                             self.initial_model_table.setCellWidget(current_row, column, reference_file_selection_combobox)
                         else:
-                            reference_file_selection_combobox=self.initial_model_dimple_dict[xtal][1]
-                            self.populate_reference_combobox(reference_file_selection_combobox)
-                            index = reference_file_selection_combobox.findText(str(reference_file[0][0]), QtCore.Qt.MatchFixedString)
-                            reference_file_selection_combobox.setCurrentIndex(index)
+                            if reference_file != []:
+                                reference_file_selection_combobox=self.initial_model_dimple_dict[xtal][1]
+                                self.populate_reference_combobox(reference_file_selection_combobox)
+                                index = reference_file_selection_combobox.findText(str(reference_file[0][0]), QtCore.Qt.MatchFixedString)
+                                reference_file_selection_combobox.setCurrentIndex(index)
                     else:
                         cell_text=QtGui.QTableWidgetItem()
                         cell_text.setText(str( db_dict[ header[1] ]  ))
