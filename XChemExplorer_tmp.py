4abf54729d75cea9df2083187de3511096c6b0ed
tkrojer <tobias.krojer@sgc.ox.ac.uk>
2016-05-06 11:45:43 +0100

    06/05/2016 - 10

--- XChemExplorer_tmp.py
+++ XChemExplorer_tmp.py
@@ -1373,7 +1373,8 @@ class XChemExplorer(QtGui.QApplication):
                 print entry
                 if entry[0]=='logfile':
                     db_dict=entry[6]
-                    if os.path.isfile(os.path.join(db_dict['DataProcessingPathToMTZfile'],db_dict['DataProcessingMTZfileName'])):
+                    if os.path.isfile(os.path.join(db_dict['DataProcessingPathToMTZfile'],db_dict['DataProcessingMTZfileName'])) or \
+                       os.path.isfile(os.path.join(db_dict['DataProcessingPathToMTZfile'])):
                         job_list=self.get_job_list_for_dimple_rerun(xtal,job_list,db_dict,entry)
         print job_list
         if job_list != []:
