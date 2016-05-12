f64bac9104b86231745b27ac1729e8d5845ea6a4
Tobias Krojer <tobiaskrojer@Tobiass-MacBook-Air.local>
2016-05-12 10:01:07 +0100

    12/05/2016 - 4

--- lib/XChemPANDDA.py
+++ lib/XChemPANDDA.py
@@ -129,10 +129,9 @@ class run_pandda_export(QtCore.QThread):
             for n,entry in enumerate(sample_dict[xtal]):
 
                 # check if EVENT map exists
-                for file in glob.glob(os.path.join(self.panddas_directory,xtal,'interesting_datasets','*ccp4')):
+                for file in glob.glob(os.path.join(self.panddas_directory,'interesting_datasets',xtal,'*ccp4')):
                     print file
 
-                print site[n]
                 for item in self.db_list:
                     if item.startswith('PANDDA_site') and site[n] in item:
                         if item.endswith('_site_index'):        db_dict[item]=entry[0]
