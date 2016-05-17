adba49b2a00164a2f3be0ab1db44c9a79bdbd48a
tkrojer <tobias.krojer@sgc.ox.ac.uk>
2016-05-17 07:49:54 +0100

    17/05/2016 - 8

--- helpers/create_png_of_compound.py
+++ helpers/create_png_of_compound.py
@@ -13,12 +13,12 @@ if __name__=='__main__':
     xtal =                      sys.argv[3]
     inital_model_directory =    sys.argv[4]
 
-    mol = Chem.MolFromSmiles(smiles)
-    AllChem.Compute2DCoords(mol)
+#    mol = Chem.MolFromSmiles(smiles)
+#    AllChem.Compute2DCoords(mol)
     # Draw to a file
 
     print 'path',os.path.join(inital_model_directory,xtal,'compound')
     os.chdir(os.path.join(inital_model_directory,xtal,'compound'))
     print 'compoundID',compoundID
     print 'mol',mol
-    Draw.MolToFile(mol, "%s.png" %compoundID.replace(' ',''))
+#    Draw.MolToFile(mol, "%s.png" %compoundID.replace(' ',''))
