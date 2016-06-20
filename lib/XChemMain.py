fe2bd1316334d5650427708b094ea6548cca5ad0
Tobias Krojer <tobiaskrojer@Tobiass-MacBook-Air.local>
2016-06-20 14:43:59 +0100

    20/06/2016 - 2

--- lib/XChemMain.py
+++ lib/XChemMain.py
@@ -2,7 +2,7 @@ import os,glob
 import sys
 import subprocess
 import getpass
-import datetime
+from datetime import datetime
 
 sys.path.append(os.path.join(os.getenv('XChemExplorer_DIR'),'lib'))
 
@@ -89,10 +89,10 @@ def get_jobs_running_on_cluster():
                 if start_time != '' and start_date != '':
                     start='%s-%s-%s %s:%s:%s' %(year_start,month_start,day_start,hour_start,minute_start,second_start)
                     print start
-                    then = datetime.datetime.strptime(start,"%Y-%m-%d %H:%M:%S")
-                    now =  datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                    delta_time = now - then
-                    print delta_time
+                    print  datetime.datetime.strptime(start,"%Y-%m-%d %H:%M:%S")-datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+#                    now =  datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+#                    delta_time = now - then
+#                    print delta_time
                 if 'dimple' in line.split()[2]:
                     dimple_jobs+=1
                     dimple_job_details.append(line.split()[0])
