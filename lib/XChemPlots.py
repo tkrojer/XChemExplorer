e8e2e12e2526b2f04419e1ecf2611868308129cf
Tobias Krojer <tobiaskrojer@Tobiass-MacBook-Air.local>
2016-07-14 17:02:40 +0100

    14/07/2016 - 34

--- lib/XChemPlots.py
+++ lib/XChemPlots.py
@@ -26,27 +26,51 @@ class summary_plot(object):
                             'Comp Chem'             ]
 
 
-
+#        self.overview_figure, self.overview_axes = plt.subplots()
 #        ax0, ax1, ax2, ax3 = self.overview_axes.flat
-        ax0 = self.overview_axes.flat
 
-        ax0.set_title('Data Collection - Outcome')
-        ax0.set_ylabel("n")
-        outcome=[]
-        frequency=[]
-        for key in db_dict:
-            if key.startswith('no_data_collection'):
-                outcome.append(key.replace('no_data_collection_',''))
-                frequency.append(db_dict[key])
-        y_pos = np.arange(len(outcome))
-        try:
-            ax0.bar(y_pos, frequency, width=0.15)
-        except ValueError:
-            pass
-        ax0.set_xticks(np.arange(len(outcome)) + 0.15/2)
-        ax0.set_xticklabels(outcome, rotation=0)
-        print 'outcome',outcome
-        print 'frequency',frequency
+#        in example
+#        fig, ax = plt.subplots()
+
+        N = 5
+        menMeans = (20, 35, 30, 35, 27)
+        menStd = (2, 3, 4, 1, 2)
+
+        ind = np.arange(N)  # the x locations for the groups
+        width = 0.35       # the width of the bars
+
+        rects1 = self.overview_axes.bar(ind, menMeans, width, color='r', yerr=menStd)
+
+        womenMeans = (25, 32, 34, 20, 25)
+        womenStd = (3, 5, 2, 3, 3)
+        rects2 = self.overview_axes.bar(ind + width, womenMeans, width, color='y', yerr=womenStd)
+
+        # add some text for labels, title and axes ticks
+        self.overview_axes.set_ylabel('Scores')
+        self.overview_axes.set_title('Scores by group and gender')
+        self.overview_axes.set_xticks(ind + width)
+        self.overview_axes.set_xticklabels(('G1', 'G2', 'G3', 'G4', 'G5'))
+
+#        ax0 = self.overview_axes.flat
+#        ax.plot
+
+#        ax0.set_title('Data Collection - Outcome')
+#        ax0.set_ylabel("n")
+#        outcome=[]
+#        frequency=[]
+#        for key in db_dict:
+#            if key.startswith('no_data_collection'):
+#                outcome.append(key.replace('no_data_collection_',''))
+#                frequency.append(db_dict[key])
+#        y_pos = np.arange(len(outcome))
+#        try:
+#            ax0.bar(y_pos, frequency, width=0.15)
+#        except ValueError:
+#            pass
+#        ax0.set_xticks(np.arange(len(outcome)) + 0.15/2)
+#        ax0.set_xticklabels(outcome, rotation=0)
+#        print 'outcome',outcome
+#        print 'frequency',frequency
 
 
 #        N = 5
