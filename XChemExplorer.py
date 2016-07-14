494b199d257a5ce9469dfb5a9d645b16bf19f2dd
Tobias Krojer <tobiaskrojer@Tobiass-MacBook-Air.local>
2016-07-14 11:49:08 +0100

    14/07/2016 - 8

--- XChemExplorer.py
+++ XChemExplorer.py
@@ -2978,7 +2978,7 @@ class XChemExplorer(QtGui.QApplication):
         if self.data_collection_dict != {}:
             if os.path.isfile(self.data_collection_summary_file):
                 self.update_log.insert('saving results to PKL file')
-#                pickle.dump(self.data_collection_dict,open(self.data_collection_summary_file,'wb'))
+                pickle.dump(self.data_collection_dict,open(self.data_collection_summary_file,'wb'))
 
         self.update_log.insert('quitting XCE... bye,bye!')
         QtGui.qApp.quit()
