227ad431671e02a5555bb6f0b6252bbb5a5ce722
Tobias Krojer <tobiaskrojer@Tobiass-MacBook-Air.local>
2016-07-28 13:28:53 +0100

    28/07/2016 - 18

--- lib/XChemDB.py
+++ lib/XChemDB.py
@@ -500,7 +500,7 @@ class data_source:
                 if not str(value).replace(' ','')=='':  # ignore if nothing in csv field
                     value_string+="'"+str(value)+"'"+','
                     column_string+=key+','
-#            print "INSERT INTO panddaTable ("+column_string[:-1]+") VALUES ("+value_string[:-1]+");"
+            print "INSERT INTO panddaTable ("+column_string[:-1]+") VALUES ("+value_string[:-1]+");"
             cursor.execute("INSERT INTO panddaTable ("+column_string[:-1]+") VALUES ("+value_string[:-1]+");")
         connect.commit()
 
