d52c09beac87f3c82a81e1ed2516727dd5ec3035
tkrojer <tobias.krojer@sgc.ox.ac.uk>
2016-08-05 13:11:17 +0100

    05/08/2016 - 3

--- lib/XChemRefine.py
+++ lib/XChemRefine.py
@@ -5,6 +5,7 @@ import os
 import glob
 import sys
 import getpass
+import fileinput
 #sys.path.append(os.path.join(os.getenv('CCP4'),'lib/python2.7/site-packages'))
 #import coot
 #sys.path.append('/usr/local/coot/SoakProc/lib')
