a7a8e991e5044782f7db9ea36ac13d20f2868fb1
tkrojer <tobias.krojer@sgc.ox.ac.uk>
2016-11-18 09:08:04 +0000

    18/11/2016

--- XChemExplorer.py
+++ XChemExplorer.py
@@ -3367,7 +3367,7 @@ class XChemExplorer(QtGui.QApplication):
             if n_datasets < int(pandda_params['min_build_datasets']):
                     self.update_log.insert('need %s datasets, but only %s are available' %(str(pandda_params['min_build_datasets']),str(n_datasets)))
             if mismatched_datasets != []:
-                self.update_log.insert('the following PDB files have a different number of atoms than the reference file:'B)
+                self.update_log.insert('the following PDB files have a different number of atoms than the reference file:')
                 for dataset in mismatched_datasets:
                     self.update_log.insert(dataset)
             self.update_log('please correct the errors and try again')
