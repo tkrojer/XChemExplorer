fa50ceb721324ddf5e47545311e41168e35d80b0
tkrojer <tobias.krojer@sgc.ox.ac.uk>
2016-12-12 09:32:23 +0000

    SGC

--- XChemExplorer.py
+++ XChemExplorer.py
@@ -2924,8 +2924,8 @@ class XChemExplorer(QtGui.QApplication):
     def run_dimple_on_selected_autoprocessing_file(self):
         job_list=[]
         for xtal in sorted(self.initial_model_dimple_dict):
-            print 'here',xtal
             if self.initial_model_dimple_dict[xtal][0].isChecked():
+                print 'here',xtal
                 db_dict=self.xtal_db_dict[xtal]
                 if os.path.isfile(os.path.join(db_dict['DataProcessingPathToMTZfile'],db_dict['DataProcessingMTZfileName'])) or \
                     os.path.isfile(os.path.join(db_dict['DataProcessingPathToMTZfile'])):
