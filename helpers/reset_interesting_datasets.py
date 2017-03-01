4bc76f67148904109fc6063b2a732fb1d2f0f48a
tkrojer <tobias.krojer@sgc.ox.ac.uk>
2017-03-01 11:13:40 +0000

    SGC

--- helpers/reset_interesting_datasets.py
+++ helpers/reset_interesting_datasets.py
@@ -20,6 +20,8 @@ def check_interesting_datasets(panddaDir):
     os.chdir(os.path.join(panddaDir,'interesting_datasets'))
     for xtal in glob.glob('*'):
         if not os.path.islink(xtal):
+            print xtal
+            print os.path.join(panddaDir,xtal,'interesting_datasets',xtal,'modelled_structures',xtal+'-pandda-model.pdb')
             if os.path.isfile(os.path.join(panddaDir,xtal,'interesting_datasets',xtal,'modelled_structures',xtal+'-pandda-model.pdb')):
                 print 'found pdb file',xtal+'-pandda-model.pdb'
 
