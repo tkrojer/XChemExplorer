19b26fd394103b02777a5a23f5648fabea4749d9
tkrojer <tobias.krojer@sgc.ox.ac.uk>
2017-03-01 10:58:59 +0000

    SGC

---
+++ helpers/reset_interesting_datasets.py
@@ -0,0 +1,28 @@
+# last edited: 01/03/2017, 15:00
+
+import os,sys
+
+def relink_interesting_datasets():
+    dirList=[]
+    for dirs in glob.glob('*'):
+        if os.path.isdir(dirs):
+            dirList.append(dirs)
+
+    for dirs in dirList:
+        print '/bin/rm -fr %s' %dirs
+        os.system('/bin/rm -fr %s' %dirs)
+        print 'ln -s ../processed_datasets/%s .' %dirs
+        os.system('ln -s ../processed_datasets/%s .' %dirs)
+
+def check_interesting_datasets(panddaDir):
+
+    os.chdir(os.path.join(panddaDir,'interesting_datasets'))
+    for folder in glob.glob('*'):
+        if not os.path.islink(folder):
+            print 'not symlink',folder
+
+
+if __name__=='__main__':
+    panddaDir=sys.argv[1]
+    check_interesting_datasets(panddaDir)
+    
