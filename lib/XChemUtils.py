18703195febafab652c9bfc9cfdd8fbb39ac86c9
tkrojer <tobias.krojer@sgc.ox.ac.uk>
2017-03-17 15:17:20 +0000

    SGC

--- lib/XChemUtils.py
+++ lib/XChemUtils.py
@@ -1562,6 +1562,7 @@ class pdbtools(object):
         spg=self.get_spg_from_pdb().replace(' ','').lower()
         if '/dls/labxchem/data/2016/lb13385-64/processing/reference/DCP2B.pdb' in self.pdb:
             print 'hereX',spg
+            print 'hereY','->'+spg+'<-'
         spg_number='0'
         for key in self.translate_spg_to_number_dict:
             if key==spg:
