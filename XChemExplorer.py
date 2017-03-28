243eadf6332f8c9ef7c55949dfa9fab755d7640a
tkrojer <tobias.krojer@sgc.ox.ac.uk>
2017-03-28 09:36:25 +0100

    SGC

--- XChemExplorer.py
+++ XChemExplorer.py
@@ -4308,7 +4308,7 @@ class XChemExplorer(QtGui.QApplication):
             msgBox.setText(msg)
             msgBox.exec_()
             return
-        elif len(cluster_dict) == 1 and reference_file == '...':
+        elif len(cluster_dict) == 1:
             reference_ID=cluster_dict.keys[0]
             reference_file=os.path.join(self.reference_directory,reference_ID+'.pdb')
             filter_pdb=''
