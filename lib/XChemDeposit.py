5bdf5f5929ebbc4fc907fecd4b2592850cd03e61
tkrojer <tobias.krojer@sgc.ox.ac.uk>
2017-05-16 14:02:26 +0100

    logging of smiles of soaked compound during apo mmcif generation

--- lib/XChemDeposit.py
+++ lib/XChemDeposit.py
@@ -814,6 +814,7 @@ class prepare_mmcif_files_for_deposition(QtCore.QThread):
         if self.structureType=='apo':
             self.Logfile.insert('SMILES string of soaked compound: %s' %smiles)
             if smiles.lower() != 'none' or smiles.lower() != "null":
+                self.Logfile.insert('adding _pdbx_entry_details.nonpolymer_details to mmcif')
                 mmcif_text+='smiles string of soaked compound: %s;\n' %smiles
             else:
                 mmcif_text=''
