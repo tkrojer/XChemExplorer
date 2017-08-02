fc26584b9cf36ad22b43946f9419dbb26418c277
tkrojer <tobias.krojer@sgc.ox.ac.uk>
2017-08-02 14:41:25 +0100

    new module for preparation of reference files

--- lib/XChemCootReference.py
+++ lib/XChemCootReference.py
@@ -629,6 +629,7 @@ class GUI(object):
                 foundLine=n
             if found and n==foundLine+2:
                 xtal=line.split(',')[0]
+                print 'map:',os.path.join(self.reference_directory,self.cb_select_mean_map.get_active_text(),'processed_datasets',xtal,xtal+'-x0149-ground-state-mean-map.native.ccp4')
                 meanmap=os.path.join(self.reference_directory,self.cb_select_mean_map.get_active_text(),'processed_datasets',xtal,xtal+'-x0149-ground-state-mean-map.native.ccp4')
                 self.ground_state_map_List.append([resolution,meanmap])
                 found=False
