6f89376aa8d4e774ddddffe654fee101d6079dc4
tkrojer <tobias.krojer@sgc.ox.ac.uk>
2017-08-02 14:58:57 +0100

    new module for preparation of reference files

---
+++ helpers/select_ground_state_dataset.py
@@ -0,0 +1,26 @@
+# last edited: 15/11/2016, 15:00
+
+import os,sys
+sys.path.append(os.path.join(os.getenv('XChemExplorer_DIR'),'lib'))
+
+import XChemDB
+
+
+# - select datasets with highest resolution
+# - select only those without an event map
+# - take the one with the lowest Rfree
+
+
+def update_data_source(db_file,xtal,db_column,status):
+    db=XChemDB.data_source(db_file)
+    db_dict={}
+    db_dict[db_column] = status
+    db.update_data_source(xtal,db_dict)
+
+if __name__=='__main__':
+    db_file=sys.argv[1]
+    xtal=sys.argv[2]
+    db_column=sys.argv[3]
+    status=sys.argv[4]
+
+    update_data_source(db_file,xtal,db_column,status)
