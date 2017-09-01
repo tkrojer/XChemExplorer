e829ee126be800f5778c7462f44b9fa4d2fca787
Skyner <reskyner@googlemail.com>
2017-09-01 12:06:11 +0100

    XChemExplorer_dmd.sh script for bashrc problem

---
+++ XChemExplorer_dmd.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export XChemExplorer_DIR="/dls/science/groups/i04-1/software/XchemExplorer/XChemExplorer"
+source $XChemExplorer_DIR/setup-scripts/xce.setup-sh
+module unload ccp4
+source /dls/science/groups/i04-1/software/pandda-update/ccp4-linux64-2016-10-22-0015/bin/ccp4.setup-sh
