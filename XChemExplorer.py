7f2fcaa6b6030cfebdf91d93d5732c2c4139414e
Skyner <reskyner@googlemail.com>
2017-09-20 15:26:45 +0100

    moved stylesheet to own script

--- XChemExplorer.py
+++ XChemExplorer.py
@@ -21,6 +21,8 @@ sys.path.append(os.path.join(os.getenv('XChemExplorer_DIR'), 'gui_scripts'))
 
 from settings_preferences import *
 from layout import *
+from stylesheet import set_stylesheet
+
 from XChemUtils import parse
 import XChemThread
 import XChemDB
@@ -45,82 +47,8 @@ class XChemExplorer(QtGui.QApplication):
         # start GUI
         self.start_GUI()
 
-        palette = QtGui.QPalette()
+        set_stylesheet(self)
 
-        palette.setColor(QtGui.QPalette.Background, QtGui.QColor("#ececec"))
-        self.setPalette(palette)
-
-        icons_directory = os.path.join((os.getenv('XChemExplorer_DIR')), 'icons')
-
-        self.setStyleSheet("""
-        QApplication {
-        padding: 0px;
-        }
-        QMenuBar {
-        background-color: rgb(236, 236, 236);
-        } 
-        QMenuBar::item {
-        background-color: rgb(236, 236, 236);
-        }
-        QMenu {
-        background-color: rgb(236, 236, 236);
-        border: 1px solid rgb(184, 192, 210);
-        }
-        QMenu::item::selected {
-        background-color: rgb(28, 105, 241);
-        color: rgb(255, 255, 255);
-        }
-        QComboBox {
-        background-color: rgb(255, 255, 255);
-        selection-background-color: rgb(28, 105, 241);
-        border: 1px solid rgb(184, 192, 210);
-        }
-        QComboBox QAbstractItemView {
-        background: rgb(255, 255, 255);
-        border: 1px solid rgb(184, 192, 210);
-        }
-        QComboBox::down-arrow {
-        image: url(""" + icons_directory + """/drop-down.png);
-        background-color: rgb(255, 255, 255);
-        }
-        QComboBox::drop-down {
-        background-color: rgb(255, 255, 255);
-        }
-        QPushButton {
-        background-color: rgb(214, 230, 244);
-        border: 1px solid rgb(184, 192, 210);
-        padding: 3px;
-        }
-        QFrame {
-        background-color: rgb(236, 236, 236);
-        }
-        QTableWidget {
-        background-color: rgb(255, 255, 255);
-        }
-        QHeaderView::section {
-        background-color: rgb(236, 236, 236);
-        border: 1px solid rgb(184, 192, 210);
-        }
-        QTabWidget::pane {
-        border-top: 1px solid rgb(184, 192, 210);
-        border-bottom: 1px solid rgb(184, 192, 210);
-        }
-        QTabBar::tab {
-        background-color: rgb(197,197,197);
-        border: 1px solid rgb(184, 192, 210);
-        padding: 3px;
-        }
-        QTabBar::tab::selected {
-        background-color: rgb(214, 230, 244);
-        border: 1px solid rgb(184, 192, 210);
-        padding: 3px;
-        }
-        QScrollBar {
-        background:  rgb(236, 236, 236);
-        }
-        """)
-
-        QtGui.qApp.setStyle('Cleanlooks')
         self.exec_()
 
     def start_GUI(self):
