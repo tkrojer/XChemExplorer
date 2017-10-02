a045c3eb450da123364cb11f5995e7d455978149
nelse003 <nelse003@gmail.com>
2017-10-02 16:30:12 +0100

    Added pipes.quote(smiles) to escape characters in smiles strings in cmdline arguments

--- lib/XChemUtils.py
+++ lib/XChemUtils.py
@@ -9,6 +9,7 @@ import getpass
 import shutil
 import math
 import platform
+import pipes
 
 from rdkit import Chem
 from rdkit.Chem import AllChem
@@ -216,13 +217,13 @@ class helpers:
             if os.path.isfile(os.path.join(initial_model_directory,sample,'old.cif')):
                 software='acedrg --res LIG -c ../old.cif -o {0!s}\n'.format((compoundID.replace(' ','')))
             else:
-                software='acedrg --res LIG -i "{0!s}" -o {1!s}\n'.format(smiles, compoundID.replace(' ',''))
+                software='acedrg --res LIG -i "{0!s}" -o {1!s}\n'.format(pipes.quote(smiles), compoundID.replace(' ',''))
         elif restraints_program=='phenix.elbow':
             if os.path.isfile(os.path.join(initial_model_directory,sample,'old.cif')):
                 software='phenix.elbow --file=../old.cif --id LIG --output {0!s}\n'.format((compoundID.replace(' ','')))
             else:
                 software='phenix.elbow --smiles="{0!s}" --id LIG --output {1!s}\n'\
-                    .format(smiles, compoundID.replace(' ',''))
+                    .format(pipes.quote(smiles), compoundID.replace(' ',''))
         elif restraints_program=='grade':
             if os.getcwd().startswith('/dls'):
                 software+='module load buster\n'
@@ -232,7 +233,7 @@ class helpers:
                     .format(compoundID.replace(' ',''), compoundID.replace(' ',''))
             else:
                 software+='grade -resname LIG -nomogul "{0!s}" -ocif {1!s}.cif -opdb {2!s}.pdb\n'\
-                    .format(smiles, compoundID.replace(' ',''), compoundID.replace(' ',''))
+                    .format(pipes.quote(smiles), compoundID.replace(' ',''), compoundID.replace(' ',''))
         # Removal of the hyrogen atoms in PDB files is required for REFMAC 5 run. With Hydrogens some ligands fail to
         # pass the external restraints in pandda.giant.make_restraints.
         # Copy the file with hydrogens to retain in case needed
@@ -240,8 +241,6 @@ class helpers:
         copy_with_hydrogens = 'cp {0!s}.pdb {0!s}_with_H.pdb'.format(compoundID.replace(' ', ''))
 
         strip_hydrogens = 'phenix.reduce {0!s}.pdb -trim {0!s}.pdb'.format(compoundID.replace(' ', ''))
-
-        print software + '\n' + copy_with_hydrogens + '\n' + strip_hydrogens
 
         Cmds = (
 
