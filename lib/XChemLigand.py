197b74be0c1bbe079d261a0308440d56e53b6cb2
Anthony Bradley <anthony.richard.bradley@gmail.com>
2017-10-08 15:24:33 +0100

    Update XChemLigand.py

--- lib/XChemLigand.py
+++ lib/XChemLigand.py
@@ -5,8 +5,7 @@ from rdkit.Chem import AllChem
 def generate_steroisomers(input_smiles):
     """
     Function to generate all stereoisomers from a SMILES String. Only works on latest version of RDKit.
-    UNTESTED
     """
     mol = Chem.MolFromSmiles(input_smiles)
-    out_mols = AllChem.GenerateStereoisomers(mol,AllChem.StereoEnumerationOptions(onlyUnassigned=False,tryEmbedding=True))
+    out_mols = AllChem.EnumerateStereoisomers(mol,AllChem.StereoEnumerationOptions(onlyUnassigned=False,tryEmbedding=True))
     return [Chem.MolToSmiles(x,isomericSmiles=True) for x in out_mols]
