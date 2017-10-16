6d1fb0ffa0eb98b7974eff4e7ee56d756c338ebb
Skyner <reskyner@googlemail.com>
2017-10-16 14:32:35 +0100

    fixed compile tests

--- compile_test.py
+++ compile_test.py
@@ -1,5 +1,4 @@
-try:
-     from XChemExplorer import *
-     print('Successfully imported XCE... compile was succesful')
-except:
-     print("Ah crap... something's not working...")
+import os
+os.system('export XChemExplorer_DIR=/XChemExplorer')
+from XChemExplorer import *
+print('Successfully imported XCE... compile was succesful')
