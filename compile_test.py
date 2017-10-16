3dcc9a479b805e0a816bb8254a0d7c2971a6c574
Skyner <reskyner@googlemail.com>
2017-10-16 15:03:43 +0100

    Hopefully fixed XCE_DIR problem when running tests

--- compile_test.py
+++ compile_test.py
@@ -1,4 +1,18 @@
-import os
-os.system('export XChemExplorer_DIR=/XChemExplorer')
+import os, sys
+
+# set XCE environment variables
+os.environ['XChemExplorer_DIR']=os.path.join(os.getcwd(), 'XChemExplorer/')
+
+print('XCE path = ' + os.environ['XChemExplorer_DIR'])
+
+# Append all directories to path
+sys.path.append(os.environ['XChemExplorer_DIR'])
+
+# print PATH to check
+print('Current path:- ')
+for p in sys.path:
+    print(p)
+
+# try to import all of XCE stuff
 from XChemExplorer import *
 print('Successfully imported XCE... compile was succesful')
