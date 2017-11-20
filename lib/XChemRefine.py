cebff2b23f1fce2f686bfd0e92b606f651a2c688
Skyner <reskyner@googlemail.com>
2017-11-20 10:59:06 +0000

    quotation mark moved

--- lib/XChemRefine.py
+++ lib/XChemRefine.py
@@ -503,12 +503,12 @@ class Refine(object):
         if external_software['qsub_remote'] != '':
             if os.path.isfile(xce_logfile): Logfile.insert('starting refinement on remote cluster')
             remote_command=external_software['qsub_remote'].replace("qsub'",'cd %s; qsub' %os.path.join(self.ProjectPath,self.xtalID,'Refine_'+Serial))
-            os.system('%s -P labxchem refmac.csh' %remote_command)
+            os.system("%s -P labxchem refmac.csh'" %remote_command)
             print '%s -P labxchem refmac.csh' %remote_command
 
         elif external_software['qsub']:
             Logfile.insert('starting refinement on cluster')
-            os.system("qsub -P labxchem refmac.csh'")
+            os.system("qsub -P labxchem refmac.csh")
 
         else:
             os.system('chmod +x refmac.csh')
