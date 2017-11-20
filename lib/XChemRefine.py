d8a3b82225e89d7c6bcdae1d08f741246618f31f
Skyner <reskyner@googlemail.com>
2017-11-20 10:52:27 +0000

    quotation mark placement in replace string

--- lib/XChemRefine.py
+++ lib/XChemRefine.py
@@ -502,13 +502,13 @@ class Refine(object):
         if os.path.isfile(xce_logfile): Logfile.insert('changing directory to %s' %(os.path.join(self.ProjectPath,self.xtalID,'Refine_'+Serial)))
         if external_software['qsub_remote'] != '':
             if os.path.isfile(xce_logfile): Logfile.insert('starting refinement on remote cluster')
-            remote_command=external_software['qsub_remote'].replace('qsub','cd %s; qsub' %os.path.join(self.ProjectPath,self.xtalID,'Refine_'+Serial))
+            remote_command=external_software['qsub_remote'].replace("qsub'",'cd %s; qsub' %os.path.join(self.ProjectPath,self.xtalID,'Refine_'+Serial))
             os.system('%s -P labxchem refmac.csh' %remote_command)
             print '%s -P labxchem refmac.csh' %remote_command
 
         elif external_software['qsub']:
             Logfile.insert('starting refinement on cluster')
-            os.system('qsub -P labxchem refmac.csh')
+            os.system("qsub -P labxchem refmac.csh'")
 
         else:
             os.system('chmod +x refmac.csh')
