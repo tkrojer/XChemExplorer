86c4a88f552548b55244d7c444db95f0fbf4885a
Skyner <reskyner@googlemail.com>
2018-01-18 15:26:44 +0000

    new path for diamond ccp4

--- XChemExplorer_dmd.sh
+++ XChemExplorer_dmd.sh
@@ -3,6 +3,6 @@
 export XChemExplorer_DIR="/dls/science/groups/i04-1/software/XChemExplorer_new/XChemExplorer"
 source $XChemExplorer_DIR/setup-scripts/xce.setup-sh
 module unload ccp4
-source /dls/science/groups/i04-1/software/pandda-update/ccp4-linux64-2016-10-22-0115/bin/ccp4.setup-sh
+source /dls/science/groups/i04-1/software/pandda-update/ccp4/ccp4-7.0/bin/ccp4.setup-sh
 
 ccp4-python $XChemExplorer_DIR/XChemExplorer.py
