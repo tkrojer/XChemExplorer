f80c91d72e922b551cebe29acc1c5d95a8228fe5
tkrojer <tobias.krojer@sgc.ox.ac.uk>
2018-02-20 14:21:46 +0000

    new autoprocssing parsing module implemented - bug fixing stage...

--- XChemExplorer.py
+++ XChemExplorer.py
@@ -3534,105 +3534,6 @@ class XChemExplorer(QtGui.QApplication):
             for key in self.initial_model_dimple_dict:
                 self.initial_model_dimple_dict[key][0].setChecked(False)
 
-    def get_sample_list_from_table(self,table):
-        sampleList = []
-        allRows = table.rowCount()
-        for row in xrange(0, allRows):
-            sample_id = str(table.item(row, 0).text())
-            sampleList.append(sample_id)
-        return sorted(sampleList)
-
-    def get_row_of_sample_in_table(self,table,xtal):
-        allRows = table.rowCount()
-        sampleRow = allRows
-        for n,row in enumerate(xrange(0, allRows)):
-            sample_id = str(table.item(row, 0).text())
-            if sample_id == xtal:
-                sampleRow = n
-                break
-        return sampleRow
-
-    def update_row_in_table(self,sample,row,db_dict,table,columns_to_show):
-        xtal = str(sample)
-        column_name = self.db.translate_xce_column_list_to_sqlite(columns_to_show)
-
-        for column, header in enumerate(column_name):
-
-            if header[0] == 'Sample ID':
-                cell_text = QtGui.QTableWidgetItem()
-                cell_text.setText(str(xtal))
-                cell_text.setTextAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignCenter)
-                table.setItem(row, column, cell_text)
-
-            elif header[0] == 'DataCollection\nOutcome':
-                if xtal not in self.dataset_outcome_combobox_dict:
-                    dataset_outcome_combobox = QtGui.QComboBox()
-                    for outcomeItem in self.dataset_outcome:
-                        dataset_outcome_combobox.addItem(outcomeItem)
-                    dataset_outcome_combobox.activated[str].connect(self.dataset_outcome_combobox_change_outcome)
-                    self.dataset_outcome_combobox_dict[xtal] = dataset_outcome_combobox
-                    table.setCellWidget(row, column, dataset_outcome_combobox)
-                index = self.dataset_outcome_combobox_dict[xtal].findText(str(db_dict['DataCollectionOutcome']), QtCore.Qt.MatchFixedString)
-                self.dataset_outcome_combobox_dict[xtal].setCurrentIndex(index)
-
-            elif header[0].startswith('img'):
-                if os.path.isfile(db_dict[header[1]]):
-                    pixmap = QtGui.QPixmap(db_dict[header[1]])
-                else:
-                    pixmap = QtGui.QPixmap(
-                        os.path.join(os.getenv('XChemExplorer_DIR'), 'image', 'IMAGE_NOT_AVAILABLE.png'))
-                image = QtGui.QLabel()
-                image.resize(128, 80)
-                image.setPixmap(pixmap.scaled(image.size(), QtCore.Qt.KeepAspectRatio))
-                table.setCellWidget(row, column, image)
-
-            elif header[0] == 'Select':
-                checkbox = QtGui.QCheckBox()
-                checkbox.toggle()
-                if table == self.deposition_table_apo:
-                    if xtal not in self.deposition_table_apo_dict:
-                        self.deposition_table_apo_dict[xtal] = checkbox
-                if table == self.deposition_table_bound:
-                    if xtal not in self.deposition_table_bound_dict:
-                        self.deposition_table_bound_dict[xtal] = checkbox
-                table.setCellWidget(row, column, checkbox)
-                checkbox.setChecked(False)
-
-            #elif header[0].startswith('SoakDB\nBarcode') or header[0].startswith('GDA\nBarcode'):
-                #                        if new_xtal:
-                #                            cell_text = QtGui.QTableWidgetItem()
-                #                            if xtal in pinDict:
-                #                                if header[0].startswith('SoakDB\nBarcode'):
-                #                                    cell_text.setText(str(pinDict[xtal][0]))
-                #                                elif header[0].startswith('GDA\nBarcode'):
-                #                                    cell_text.setText(str(pinDict[xtal][1]))
-                #                                if pinDict[xtal][0] == 'NULL' or pinDict[xtal][1] == 'NULL':
-                #                                    cell_text.setBackground(QtGui.QColor(255, 215, 0))
-                #                                elif pinDict[xtal][0] != pinDict[xtal][1]:
-                #                                    cell_text.setBackground(QtGui.QColor(255, 0, 0))
-                #                            else:
-                #                                cell_text.setText('')
-                #                            cell_text.setTextAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignCenter)
-                #                            self.datasets_summary_table.setItem(current_row, column, cell_text)
-            else:
-                cell_text = QtGui.QTableWidgetItem()
-                # in case data collection failed for whatever reason
-                try:
-                    cell_text.setText(str(db_dict[header[1]]))
-                except KeyError:  # older pkl files may not have all the columns
-                    cell_text.setText('n/a')
-                    #                        else:
-                    #                            if header[0].startswith('Resolution\n[Mn<I/sig(I)> = 1.5]'):
-                    #                                cell_text.setText('999')
-                    #                            elif header[0].startswith('DataProcessing\nRfree'):
-                    #                                cell_text.setText('999')
-                    #                            elif header[0].startswith('Rmerge\nLow'):
-                    #                                cell_text.setText('999')
-                    #                            else:
-                    #                                cell_text.setText('')
-                cell_text.setTextAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignCenter)
-                table.setItem(row, column, cell_text)
-
 
     def show_data_collection_details(self, state):
         # first remove currently displayed widget
@@ -3843,6 +3744,106 @@ class XChemExplorer(QtGui.QApplication):
     # => new data collection summary table
     # > start
 
+    def get_sample_list_from_table(self,table):
+        sampleList = []
+        allRows = table.rowCount()
+        for row in xrange(0, allRows):
+            sample_id = str(table.item(row, 0).text())
+            sampleList.append(sample_id)
+        return sorted(sampleList)
+
+    def get_row_of_sample_in_table(self,table,xtal):
+        allRows = table.rowCount()
+        sampleRow = allRows
+        for n,row in enumerate(xrange(0, allRows)):
+            sample_id = str(table.item(row, 0).text())
+            if sample_id == xtal:
+                sampleRow = n
+                break
+        return sampleRow
+
+    def update_row_in_table(self,sample,row,db_dict,table,columns_to_show):
+        xtal = str(sample)
+        column_name = self.db.translate_xce_column_list_to_sqlite(columns_to_show)
+
+        for column, header in enumerate(column_name):
+
+            if header[0] == 'Sample ID':
+                cell_text = QtGui.QTableWidgetItem()
+                cell_text.setText(str(xtal))
+                cell_text.setTextAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignCenter)
+                table.setItem(row, column, cell_text)
+
+            elif header[0] == 'DataCollection\nOutcome':
+                if xtal not in self.dataset_outcome_combobox_dict:
+                    dataset_outcome_combobox = QtGui.QComboBox()
+                    for outcomeItem in self.dataset_outcome:
+                        dataset_outcome_combobox.addItem(outcomeItem)
+                    dataset_outcome_combobox.activated[str].connect(self.dataset_outcome_combobox_change_outcome)
+                    self.dataset_outcome_combobox_dict[xtal] = dataset_outcome_combobox
+                    table.setCellWidget(row, column, dataset_outcome_combobox)
+                index = self.dataset_outcome_combobox_dict[xtal].findText(str(db_dict['DataCollectionOutcome']), QtCore.Qt.MatchFixedString)
+                self.dataset_outcome_combobox_dict[xtal].setCurrentIndex(index)
+
+            elif header[0].startswith('img'):
+                if os.path.isfile(db_dict[header[1]]):
+                    pixmap = QtGui.QPixmap(db_dict[header[1]])
+                else:
+                    pixmap = QtGui.QPixmap(
+                        os.path.join(os.getenv('XChemExplorer_DIR'), 'image', 'IMAGE_NOT_AVAILABLE.png'))
+                image = QtGui.QLabel()
+                image.resize(128, 80)
+                image.setPixmap(pixmap.scaled(image.size(), QtCore.Qt.KeepAspectRatio))
+                table.setCellWidget(row, column, image)
+
+            elif header[0] == 'Select':
+                checkbox = QtGui.QCheckBox()
+                checkbox.toggle()
+                if table == self.deposition_table_apo:
+                    if xtal not in self.deposition_table_apo_dict:
+                        self.deposition_table_apo_dict[xtal] = checkbox
+                if table == self.deposition_table_bound:
+                    if xtal not in self.deposition_table_bound_dict:
+                        self.deposition_table_bound_dict[xtal] = checkbox
+                table.setCellWidget(row, column, checkbox)
+                checkbox.setChecked(False)
+
+            #elif header[0].startswith('SoakDB\nBarcode') or header[0].startswith('GDA\nBarcode'):
+                #                        if new_xtal:
+                #                            cell_text = QtGui.QTableWidgetItem()
+                #                            if xtal in pinDict:
+                #                                if header[0].startswith('SoakDB\nBarcode'):
+                #                                    cell_text.setText(str(pinDict[xtal][0]))
+                #                                elif header[0].startswith('GDA\nBarcode'):
+                #                                    cell_text.setText(str(pinDict[xtal][1]))
+                #                                if pinDict[xtal][0] == 'NULL' or pinDict[xtal][1] == 'NULL':
+                #                                    cell_text.setBackground(QtGui.QColor(255, 215, 0))
+                #                                elif pinDict[xtal][0] != pinDict[xtal][1]:
+                #                                    cell_text.setBackground(QtGui.QColor(255, 0, 0))
+                #                            else:
+                #                                cell_text.setText('')
+                #                            cell_text.setTextAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignCenter)
+                #                            self.datasets_summary_table.setItem(current_row, column, cell_text)
+            else:
+                cell_text = QtGui.QTableWidgetItem()
+                # in case data collection failed for whatever reason
+                try:
+                    cell_text.setText(str(db_dict[header[1]]))
+                except KeyError:  # older pkl files may not have all the columns
+                    cell_text.setText('n/a')
+                    #                        else:
+                    #                            if header[0].startswith('Resolution\n[Mn<I/sig(I)> = 1.5]'):
+                    #                                cell_text.setText('999')
+                    #                            elif header[0].startswith('DataProcessing\nRfree'):
+                    #                                cell_text.setText('999')
+                    #                            elif header[0].startswith('Rmerge\nLow'):
+                    #                                cell_text.setText('999')
+                    #                            else:
+                    #                                cell_text.setText('')
+                cell_text.setTextAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignCenter)
+                table.setItem(row, column, cell_text)
+
+
     def populate_datasets_summary_table_NEW(self):
         self.status_bar.showMessage(
             'Building summary table for data processing results; be patient this may take a while')
@@ -3954,6 +3955,7 @@ class XChemExplorer(QtGui.QApplication):
         else:
             print 'nothing to change'
         self.msgBox.done(1)
+
 
 
     # < end
