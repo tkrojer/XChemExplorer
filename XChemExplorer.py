fc1463041122e06025d2639369f6ff8b23b1f780
Skyner <reskyner@googlemail.com>
2018-02-26 14:53:12 +0000

    added .pdb to reference file root to allow filtering by pdb

--- XChemExplorer.py
+++ XChemExplorer.py
@@ -3480,7 +3480,9 @@ class XChemExplorer(QtGui.QApplication):
             for files in glob.glob(self.reference_directory + '/*'):
                 if files.endswith('.pdb'):
                     reference_root = files[files.rfind('/') + 1:files.rfind('.')]
+
                     if os.path.isfile(os.path.join(self.reference_directory, reference_root + '.pdb')):
+                        reference_file = reference_root + '.pdb'
                         pdb_reference = parse().PDBheader(
                             os.path.join(self.reference_directory, reference_root + '.pdb'))
                         spg_reference = pdb_reference['SpaceGroup']
@@ -3488,7 +3490,7 @@ class XChemExplorer(QtGui.QApplication):
                         lattice_reference = pdb_reference['Lattice']
                         unitcell_volume_reference = pdb_reference['UnitCellVolume']
                         pointgroup_reference = pdb_reference['PointGroup']
-                        reference_file_list.append([reference_root,
+                        reference_file_list.append([reference_file,
                                                     spg_reference,
                                                     unitcell_reference,
                                                     lattice_reference,
