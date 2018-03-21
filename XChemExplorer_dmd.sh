adeadee3f0b733089e464d016177426fe0682a61
Rachael Skyner <rachael.skyner@diamond.ac.uk>
2018-03-21 10:37:31 +0000

    Update XChemExplorer_dmd.sh
    Change path for XCE location

--- XChemExplorer_dmd.sh
+++ XChemExplorer_dmd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export XChemExplorer_DIR="/home/zqr16691/tmp/XChemExplorer"
-source $XChemExplorer_DIR/setup-scripts/xce.setup-sh
+export XChemExplorer_DIR="/dls/science/groups/i04-1/software/XChemExplorer_new/XChemExplorer"
+source $XChemExplorer_DIR/setup-scripts/xce.setup-shXChemExplorer
 module unload ccp4
 source /dls/science/groups/i04-1/software/pandda-update/ccp4/ccp4-7.0/bin/ccp4.setup-sh
 
