3d9e63fc2e7fea1d6ab2274e09845a77fb4ddadc
Rachael Skyner <rachael.skyner@diamond.ac.uk>
2018-03-21 14:14:44 +0000

    Update XChemExplorer_dmd.sh

--- XChemExplorer_dmd.sh
+++ XChemExplorer_dmd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export XChemExplorer_DIR="/dls/science/groups/i04-1/software/XChemExplorer_new/XChemExplorer"
-source $XChemExplorer_DIR/setup-scripts/xce.setup-shXChemExplorer
+source $XChemExplorer_DIR/setup-scripts/xce.setup-sh
 module unload ccp4
 source /dls/science/groups/i04-1/software/pandda-update/ccp4/ccp4-7.0/bin/ccp4.setup-sh
 
